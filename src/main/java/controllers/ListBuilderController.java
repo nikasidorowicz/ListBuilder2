e8217169fe8306215443beed65ae7c47cd1479f3
nikasidorowicz <dominika.sidorowicz@gmail.com>
2018-06-26 23:24:15 +0200

    Develop ListBuilderController for main application view.
    * Bind an example placeholder for list summary.

--- src/main/java/controllers/ListBuilderController.java
+++ src/main/java/controllers/ListBuilderController.java
@@ -1,31 +1,47 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import objects.Components;
 
+@Data
 public class ListBuilderController implements HierarchicalController {
 
     @FXML
-    private TextFlow listSummary;
+    protected TextFlow listSummary;
 
     @FXML
     private ChoiceBox<String> sideOfConflict;
     @FXML
     private TextField pointsLimit;
 
-    @Getter
-    @Setter
     private Components components;
 
     public ListBuilderController() {
         String gameVersion = "testVersion";
         setComponents(new Components(gameVersion));
-//        pointsLimit = "0";
+
+        // Load list summary pane
+//        FXMLLoader listSummaryLoader = new FXMLLoader(getClass().getResource("ListSummary.fxml"));
+
+        Text testSummary = new Text("THIS IS SOME SUMMARY");
+        testSummary.setFill(Color.RED);
+        testSummary.setFont(Font.font("Helvetica", FontPosture.ITALIC, 40));
+    }
+
+    public void initialize() {
+        Text testSummary = new Text("THIS IS SOME SUMMARY");
+        listSummary.getChildren().add(testSummary);
     }
 
     @Override
